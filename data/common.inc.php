8d431689e26b8aa53b4a4acd73a7440f11d1decc
milkbobo <mileto@qq.com>
2015-06-02 10:15:46 +0800

    new
    顺辰cms项目

---
+++ data/common.inc.php
@@ -0,0 +1,11 @@
+<?php
+//数据库连接信息
+$cfg_dbhost = 'rdsjm7rqf3qrfuy.mysql.rds.aliyuncs.com';
+$cfg_dbname = 'shunchen';
+$cfg_dbuser = 'shunchen';
+$cfg_dbpwd = 'shunchenvip';
+$cfg_dbprefix = 'dede_';
+$cfg_db_language = 'utf8';
+
+
+?>
